05bae717e95ec509634531a7698909b19d155b33
CodeKageFX <luqmanola60@gmail.com>
2026-04-11 09:39:19 +0100

    feat: styled todo card with dark theme, accessibility and interactions

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import TodoCard from "./components/TodoCard";
 
 export default function App() {
   return (
-    <div className="w-fit mx-auto">
-      <TodoCard />
-    </div>
+    <TodoCard />
   )
 }
